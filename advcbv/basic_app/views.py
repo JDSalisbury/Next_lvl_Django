b653431e4d8002e6e8c90b0a4d4ffeb9e16d1b7c
Jeffrey Salisbury <jsalisbury@everest.local>
2018-10-17 11:53:55 -0400

    added injection from a dictionary function, some comments explain more of whats going on with the **kwargs

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
+#python **kwargs key word arguments, will accept any number of arguments for a dictionary/hash map
+# https://stackoverflow.com/questions/36901/what-does-double-star-asterisk-and-star-asterisk-do-for-parameters
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = "Basic Injection!"
+        return context
 
 
 
