40f8e736fbe2b4217f52463366e7d8fadb4672dd
Jeffrey Salisbury <jsalisbury@everest.local>
2018-10-17 13:33:40 -0400

    added the views to see the list of said school and students going to the school. also placed html files into basic app to keep it seperate from rest of project. to make more modular.

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, List, DetailView
+from . import models
 # from django.http import HttpResponse
 
 # Create your views here.
 #python **kwargs key word arguments, will accept any number of arguments for a dictionary/hash map
 # https://stackoverflow.com/questions/36901/what-does-double-star-asterisk-and-star-asterisk-do-for-parameters
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+#
+#     def get_context_data(self,**kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['injectme'] = "Basic Injection!"
+#         return context
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+class SchoolListView(ListView):
+    model = models.School
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = "Basic Injection!"
-        return context
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+
 
 
 
