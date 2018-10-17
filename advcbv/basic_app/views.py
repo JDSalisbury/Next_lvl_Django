ec762905667f34eb6d22eab7f5b6c590258a903b
Jeffrey Salisbury <jsalisbury@everest.local>
2018-10-17 14:09:10 -0400

    listview not list

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, List, DetailView
+from django.views.generic import View, TemplateView, ListView, DetailView
 from . import models
 # from django.http import HttpResponse
 
